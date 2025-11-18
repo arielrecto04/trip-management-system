dbaef50ff539e34e1ee2ec772cd2e76518a11847
Cesar Pinero <cpinero522003@gmail.com>
2025-11-18 09:46:56 +0800

    update: wrong spelling

--- database/migrations/2025_11_17_022025_create_vehicles_table.php
+++ database/migrations/2025_11_17_022025_create_vehicles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Trip::class, 'current_trip_id')->nullalbe();
+            $table->foreignIdFor(Trip::class, 'current_trip_id')->nullable();
             $table->string('vin')->unique();
             $table->string('brand');
             $table->string('model');
